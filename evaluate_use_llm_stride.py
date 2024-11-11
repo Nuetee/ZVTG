63ed78c22cdb8b10173675558193ad4a9f02bd53
Nuetee <sungjoon729@gmail.com>
2024-11-11 17:52:39 +0900

    TFVTG use llm -stride

---
+++ evaluate_use_llm_stride.py
@@ -0,0 +1,90 @@
+from data_configs import DATASETS
+import argparse
+import numpy as np
+import json
+from tqdm import tqdm
+from vlm_localizer import localize
+from qvhhighlight_eval import eval_submission
+import os
+from llm_prompting import select_proposal, filter_and_integrate
+import pdb
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Evaluation for training-free video temporal grounding.')
+    parser.add_argument('--dataset', default='charades', type=str, help='Specify the dataset. See supported datasets in data_configs.py.')
+    parser.add_argument('--split', default='default', type=str, help='Specify the split. See supported splits in data_configs.py.')
+    parser.add_argument('--llm_output', default=None, type=str, help='LLM prompt output. If not specified, use only VLM for evaluation.')
+
+    return parser.parse_args()
+
+
+def calc_iou(candidates, gt):
+    start, end = candidates[:,0], candidates[:,1]
+    s, e = gt[0], gt[1]
+    inter = np.minimum(end, e) - np.maximum(start, s)
+    union = np.maximum(end, e) - np.minimum(start, s)
+    return inter.clip(min=0) / union
+
+def eval_with_llm(data, feature_path, pad_sec=0.0):
+    ious = []
+    thresh = np.array([0.3, 0.5, 0.7])
+    recall = np.array([0, 0, 0])
+    stride = 40
+    max_stride_factor = 1
+    
+
+    pbar = tqdm(data.items())
+    for vid, ann in pbar:
+        duration = ann['duration']
+        video_feature = np.load(os.path.join(feature_path, vid+'.npy'))
+
+        for i in range(len(ann['sentences'])):
+            # sub queries
+            sub_query_proposals = []
+            if 'query_json' in ann['response'][i]:
+                relation = ann['response'][i]['relationship']
+                for j in range(1, len(ann['response'][i]['query_json'])):
+                    query_json = [{'descriptions': q} for q in ann['response'][i]['query_json'][j]['descriptions']]
+                    answers = localize(video_feature, duration, query_json, stride, int(video_feature.shape[0] * max_stride_factor))
+                    proposals = []
+                    for t in range(3):
+                        proposals += [[p['response'][t]['start'], p['response'][t]['end'], p['response'][t]['confidence']] for p in answers if len(p['response']) > t]
+                    proposals = np.array(proposals)
+                    sub_query_proposals.append(select_proposal(proposals)[:3])
+            else:
+                relation = 'single-query'
+            # query
+            query_json = [{'descriptions': ann['sentences'][i]}]
+            if 'query_json' in ann['response'][i] and len(ann['response'][i]['query_json']) > 0:
+                query_json += [{'descriptions': q} for q in ann['response'][i]['query_json'][0]['descriptions']]
+            answers = localize(video_feature, duration, query_json, stride, int(video_feature.shape[0] * max_stride_factor))
+            proposals = []
+            for t in range(3):
+                proposals += [[p['response'][t]['start'], p['response'][t]['end'], p['response'][t]['confidence']] for p in answers if len(p['response']) > t]
+            proposals = proposals[:7] + filter_and_integrate(sub_query_proposals, relation)
+            proposals = select_proposal(np.array(proposals))
+                
+            gt = ann['timestamps'][i]
+            iou_ = calc_iou(proposals[:1], gt)[0]
+            ious.append(max(iou_, 0))
+            recall += thresh <= iou_
+
+        pbar.set_postfix({"mIoU": sum(ious) / len(ious), 'recall': str(recall / len(ious))})
+
+    print('mIoU:', sum(ious) / len(ious))
+    for th, r in zip(thresh, recall):
+        print(f'R@{th}:', r / len(ious))
+
+if __name__=='__main__':
+    args = get_args()
+    assert args.dataset in DATASETS, 'Unsupported dataset. To evaluate other datasets, please add the configuration in data_configs.py.'
+    dataset = DATASETS[args.dataset]
+    assert args.split in dataset['splits'], 'Unsupported split. To evaluate other split, please add the configuration in data_configs.py.'
+    
+    print('Evaluating', args.dataset, args.split)
+    
+    with open(args.llm_output) as f:
+        data = json.load(f)
+    eval_with_llm(data, dataset['feature_path'], dataset['splits'][args.split]['pad_sec'])
+
+        
