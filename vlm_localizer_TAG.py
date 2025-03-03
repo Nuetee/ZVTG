c34e2f962a77e801c4af2a0a5a76c704439b1dac
Nuetee <sungjoon729@gmail.com>
2025-03-03 11:22:51 +0000

    extract score 수정

--- vlm_localizer_TAG.py
+++ vlm_localizer_TAG.py
@@ -267,8 +267,8 @@ def extract_static_score(start, end, cum_scores, num_frames, scores):
     if kernel_size != num_frames:
         static_score = inner_sum / kernel_size - outer_sum / (num_frames - kernel_size)
     else:
-        # static_score = inner_sum / kernel_size - (scores[0][0] + scores[0][-1] / 2)
-        static_score = inner_sum / kernel_size
+        static_score = inner_sum / kernel_size - (scores[0][0] + scores[0][-1]) / 2
+        # static_score = inner_sum / kernel_size
     return static_score
 
 
@@ -504,7 +504,7 @@ def generate_proposal_revise(video_features, sentences, stride, hyperparams, kme
 
     # proposal generation by using scene segments integration
     cum_scores = torch.cumsum(normalized_scores, dim=1)[0]
-    final_proposals, final_proposals_static_score = get_proposals_with_scores(scene_segments, cum_scores, normalized_scores, num_frames, hyperparams['prior'])
+    final_proposals, final_proposals_static_score = get_proposals_with_scores(scene_segments, cum_scores, scores, num_frames, hyperparams['prior'])
 
     final_proposals = [
         [
