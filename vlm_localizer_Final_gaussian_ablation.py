760e3f4d2f01296e0c6815d1cf0672220d374ea3
Nuetee <sungjoon729@gmail.com>
2025-05-15 17:53:28 +0900

    SA ablation

--- vlm_localizer_Final_gaussian_ablation.py
+++ vlm_localizer_Final_gaussian_ablation.py
@@ -341,44 +341,6 @@ def alignment_adjustment(data, scale_gamma, device, lambda_max=2, lambda_min=-2)
     return normalized_scores, is_scale
 
 
-def alignment_adjustment_yeo_johnson(data, scale_gamma, device, lambda_max=2, lambda_min=-2):
-    def yeo_johnson_transformed(x, lmbda):
-        if lmbda == 0:
-            return np.sign(x) * np.log1p(np.abs(x))
-        else:
-            pos_part = (np.power(x + 1, lmbda) - 1) / lmbda if x >= 0 else 0
-            neg_part = -(np.power(-x + 1, 2 - lmbda) - 1) / (2 - lmbda) if x < 0 else 0
-            return pos_part + neg_part
-
-    # 최적의 lambda를 찾기 위한 로그 가능도 함수 (최소화할 함수)
-    def neg_log_likelihood(lmbda):
-        transformed_data = np.array([yeo_johnson_transformed(x, lmbda) for x in data])
-        var = np.var(transformed_data, ddof=1)
-        return -np.sum(np.log(np.abs(transformed_data) + 1e-9)) + 0.5 * len(data) * np.log(var)
-
-    # lambda 범위 내에서 최적화
-    result = minimize_scalar(neg_log_likelihood, bounds=(lambda_min, lambda_max), method='bounded')
-    best_lambda = result.x
-
-    # 최적의 lambda로 변환 데이터 생성
-    transformed_data = np.array([yeo_johnson_transformed(x, best_lambda) for x in data])
-
-    original_min, original_max = data.min(), data.max()
-    transformed_min, transformed_max = transformed_data.min(), transformed_data.max()
-    transformed_data = (transformed_data - transformed_min) / (transformed_max - transformed_min)  # normalize to [0, 1]
-    is_scale = False
-    if original_max - original_min > scale_gamma:
-        is_scale = True
-        transformed_data = transformed_data * (original_max - original_min) + original_min  # scale to original min/max
-    else:
-        transformed_data = transformed_data * (scale_gamma) + original_min
-
-    # 변환 결과를 다시 텐서로 변환하고 원래 형태로 복원
-    normalized_scores = torch.tensor(transformed_data, device=device).unsqueeze(0)
-
-    return normalized_scores, is_scale
-
-
 def temporal_aware_feature_smoothing(kernel_size, features):
     padding_size = kernel_size // 2
     padded_features = torch.cat((features[0].repeat(padding_size, 1), features, features[-1].repeat(padding_size, 1)), dim=0)
@@ -577,7 +539,7 @@ def generate_proposal_revise(video_features, sentences, stride, hyperparams, tck
     # Alignment adjustment of similarity scores
     data = scores[:, masks].flatten().cpu().numpy()   # 마스크된 부분만 가져오기    
     # normalized_scores, is_scale = alignment_adjustment(data, hyperparams['gamma'], scores.device, lambda_max=2, lambda_min=-2)
-    normalized_scores, is_scale = alignment_adjustment_yeo_johnson(data, hyperparams['gamma'], scores.device, lambda_max=2, lambda_min=-2)
+    normalized_scores, is_scale = alignment_adjustment(data, hyperparams['gamma'], scores.device, lambda_max=2, lambda_min=-2)
     
     ### w/o AN ###
     # data = scores[:, masks].flatten().cpu().numpy()   # 마스크된 부분만 가져오기    
